d8ed8be4b10260261b1fdce4932b4a7b70464d8a
Iryna Ielkina <40836100+ielkina@users.noreply.github.com>
2025-03-05 10:44:36 +0300

    update project files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import postcssImport from "postcss-import";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import combineDuplicatedSelectors from "postcss-combine-duplicated-selectors";
-import path from "path";
+// import { environments } from "eslint-plugin-vue";
+// import path from "path";
 
 const ReactCompilerConfig = {
   presets: ["@babel/preset-react"],
@@ -16,7 +17,7 @@ const ReactCompilerConfig = {
 
 export default defineConfig(() => {
   return {
-    base: "/react-vite/",
+    base: "/react-vite",
     plugins: [
       react({
         babel: {
@@ -24,18 +25,23 @@ export default defineConfig(() => {
         },
       }),
     ],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./src/main.jsx"],
+    },
     resolve: {
       alias: {
         //настройка относительных путей в проекте
-        src: path.resolve(__dirname, "src"),
-        App: path.resolve(__dirname, "src/App/"),
-        components: path.resolve(__dirname, "src/App/components/"),
-        data: path.resolve(__dirname, "src/data/"),
-        icons: path.resolve(__dirname, "src/icons/"),
-        img: path.resolve(__dirname, "src/img/"),
-        scss: path.resolve(__dirname, "src/scss/"),
-        services: path.resolve(__dirname, "src/services/"),
-        style: path.resolve(__dirname, "src/style/"),
+        // src: path.resolve(__dirname, "src"),
+        // App: path.resolve(__dirname, "src/App/"),
+        // components: path.resolve(__dirname, "src/App/components/"),
+        // data: path.resolve(__dirname, "src/data/"),
+        // icons: path.resolve(__dirname, "src/icons/"),
+        // img: path.resolve(__dirname, "src/img/"),
+        // scss: path.resolve(__dirname, "src/scss/"),
+        // services: path.resolve(__dirname, "src/services/"),
+        // style: path.resolve(__dirname, "src/style/"),
       },
       extensions: [".js", ".jsx", ".json", ".css", ".scss", ".sass"],
     },
